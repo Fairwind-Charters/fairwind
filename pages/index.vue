7ff405fe210aa87204ac0fd76753c0c2f425ec53
Didac Bigorda <didacbigorda@gmail.com>
2021-03-11 22:50:16 -0500

    Images

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,21 @@
       class="h-screen"
     >
       <div class="h-screen flex flex-col justify-center items-center bg-black bg-opacity-20">
-        <h1 class="title font-logo sm:text-8xl text-5xl text-white">Fairwind Charters</h1>
+        <h1 class="title font-logo md:text-9xl sm:text-7xl text-5xl text-white">Fairwind Charters</h1>
         <h2 class="text-white sm:text-2xl">Private yacht charters for all occassions</h2>
+        <nuxt-link
+          to="/contact"
+          class="mt-4 whitespace-nowrap inline-flex whitespace-nowrap
+            items-center justify-center px-8 py-2 border text-xl
+            border-transparent rounded-md shadow-sm
+            font-medium text-white bg-fairwind-pink-100 hover:bg-fairwind-pink-200"
+        >
+          Contact Us
+        </nuxt-link>
       </div>
     </video-background>
-    <section id="tours">
+    <section id="tours" class="w-screen">
+      <Tours />
     </section>
     <section id="instagram">
     </section>
@@ -25,8 +35,12 @@
 </template>
 
 <script>
+import Tours from '~/components/general/Tours'
 export default {
   layout: 'home',
+  components: {
+    Tours,
+  },
   data() {
     return {
       src: ``
