89a76792d9a7536c5cece204defa63419bccc849
Didac Bigorda <didacbigorda@gmail.com>
2021-03-29 08:01:10 -0400

    Add contact form field phone

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -53,17 +53,24 @@
           </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
               E-mail
             </label>
             <input v-model="email" :class="[ emailError ? 'border-red-500' : 'border-gray-200' ]"  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" name="email" type="email">
             <p :class="[ emailError ? 'block' : 'hidden' ]" class="text-red-500 text-xs italic">Please fill out this field</p>
           </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-phone">
+              Last Name
+            </label>
+            <input v-model="lastname" :class="[ lastnameError ? 'border-red-500' : 'border-gray-200' ]"  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="phone" name="phone" type="phone" placeholder="Doe">
+            <p :class="[ lastnameError ? 'block' : 'hidden' ]" class="text-red-500 text-xs italic hidden">Please fill out this field.</p>
+          </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-message">
               Message
             </label>
             <textarea v-model="msg" :class="[ msgError ? 'border-red-500' : 'border-gray-200' ]"  class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" name="message"></textarea>
